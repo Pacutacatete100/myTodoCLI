459c44b9e77a8ab45fb137b8667fd1f2644b2f2d
Pacutacatete100 <pacutacatete100@gmail.com>
2020-08-12 02:05:02 -0400

    added class parameter

--- todo/TodoItem.py
+++ todo/TodoItem.py
@@ -1,13 +1,14 @@
 class Todo_Item:
-    def __init__(self, is_done, name, due_date, number, is_done_check='[ ]'):
+    def __init__(self, is_done, name, due_date, number, class_name='misc', is_done_check='[ ]'):
         self.is_done = is_done
         self.name = name
         self.due_date = due_date
         self.number = number
         self.is_done_check = is_done_check if is_done_check is not None else '[ ]'
+        self.class_name = class_name if class_name is not None else 'misc'
 
     def __str__(self):
-        return str(self.number) + '. ' + self.is_done_check + ' ' + self.name + '. Due: ' + self.due_date
+        return str(self.number) + '. ' + self.is_done_check + ' ' + self.name + '. Due: ' + self.due_date + '. Class: ' + self.class_name
 
     def mark_as_completed(self):
         self.is_done_check = '[x]'
--- todo/todo.py
+++ todo/todo.py
@@ -3,9 +3,10 @@ import click
 from TodoItem import Todo_Item
 import datetime
 
-initital_todo_list = [Todo_Item(False, "Fill up this list", "tomorrow", 1), 
-                    Todo_Item(False, "another item for testing", "tomorrow", 2)
-                    ]
+todo_list = [Todo_Item(False, "Fill up this list", "tomorrow", 1), 
+             Todo_Item(False, "another item for testing", "tomorrow", 2),
+             Todo_Item(False, "100 pushups", "tomorrow", 3)
+            ]
 
 @click.group('todo')
 def main():
@@ -14,20 +15,20 @@ def main():
 @main.command('list') 
 def list():
     click.echo('--------------- TODO LIST ---------------')
-    for ti in initital_todo_list:
+    for ti in todo_list:
         click.echo(ti)
     click.echo('')
 
 @main.command('add')
 @click.option('--item', prompt=True)
 @click.option('--due', prompt=True)
-def add(item, due):
-    initital_todo_list.append(Todo_Item(False, item, due, len(initital_todo_list) + 1))
+@click.option('--classname', prompt='Class Name')
+def add(item, due, classname):
+    todo_list.append(Todo_Item(False, item, due, len(todo_list) + 1, classname))
     click.echo('------------ TODO LIST ------------')
-    for ti in initital_todo_list:
+    for ti in todo_list:
         click.echo(ti)
     click.echo('')
-    
 
 if __name__ == '__main__':
     main()
